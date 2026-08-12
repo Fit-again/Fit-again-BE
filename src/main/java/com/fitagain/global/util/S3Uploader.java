814aef7686da8a9e2c2c52cf7426135919c9813d
sondg921 <sondg921@naver.com>
2026-08-12 18:53:35 +0900

    feat: AWS S3 파일 업로드 환경 구축

---
+++ src/main/java/com/fitagain/global/util/S3Uploader.java
@@ -0,0 +1,49 @@
+package com.fitagain.global.util;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class S3Uploader {
+
+    private final S3Client s3Client;
+
+    @Value("${spring.cloud.aws.s3.bucket}")
+    private String bucket;
+
+    /**
+     * S3에 파일을 업로드하고 퍼블릭 URL을 반환합니다.
+     * @param multipartFile 업로드할 파일
+     * @param dirName S3 내부에 저장될 폴더 이름 (예: "tasks/front")
+     * @return S3 객체 URL
+     */
+    public String upload(MultipartFile multipartFile, String dirName) throws IOException {
+
+        String originalFileName = multipartFile.getOriginalFilename();
+        // 파일 이름이 겹치지 않도록 UUID 추가
+        String uniqueFileName = dirName + "/" + UUID.randomUUID() + "_" + originalFileName;
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucket)
+                .key(uniqueFileName)
+                .contentType(multipartFile.getContentType())
+                .build();
+
+        // S3로 파일 전송
+        s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(multipartFile.getInputStream(), multipartFile.getSize()));
+
+        // 생성된 퍼블릭 URL 반환
+        return "https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/" + uniqueFileName;
+    }
+}
