0a9dab0267eee6a8ab2f7bf9fd51fa9f7bcfbd56
sondg921 <sondg921@naver.com>
2026-08-12 22:19:54 +0900

    feat: service Transactional 어노테이션 추가

--- src/main/java/com/fitagain/domain/image/service/command/ImageCommandServiceImpl.java
+++ src/main/java/com/fitagain/domain/image/service/command/ImageCommandServiceImpl.java
@@ -5,6 +5,7 @@ import com.fitagain.global.util.S3Uploader;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -12,6 +13,7 @@ import java.io.IOException;
 @Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class ImageCommandServiceImpl implements ImageCommandService {
 
     private final S3Uploader s3Uploader;
